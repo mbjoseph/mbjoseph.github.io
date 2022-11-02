aa44cd9f5a0a88bb626a3ef8bfed9af778219d09
Max Joseph <maxwellbjoseph@gmail.com>
2022-11-01 22:14:11 -0600

    More updates

--- main.R
+++ main.R
@@ -1,9 +1,9 @@
 library(rmarkdown)
 
 clean_site()
-post_rmd_files <- list.files('_posts', 
-                             pattern = '.Rmd', 
-                             recursive = TRUE, 
+post_rmd_files <- list.files('_posts',
+                             pattern = '.Rmd',
+                             recursive = TRUE,
                              full.names = TRUE)
 for (i in seq_along(post_rmd_files)) {
   render(post_rmd_files[i])
